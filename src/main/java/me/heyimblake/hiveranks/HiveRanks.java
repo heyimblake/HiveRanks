b59879d32a1b43118bdf2ffd5f3ff5edf2926502
heyimblake <blake@heyimblake.me>
2017-03-18 23:00:24 -0400

    Add bStats (Metrics)

--- src/main/java/me/heyimblake/hiveranks/HiveRanks.java
+++ src/main/java/me/heyimblake/hiveranks/HiveRanks.java
@@ -2,12 +2,14 @@ package me.heyimblake.hiveranks;
 
 import me.heyimblake.hiveranks.commands.HiveRanksCommand;
 import me.heyimblake.hiveranks.listeners.PlayerJoinListener;
+import org.bstats.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HiveRanks extends JavaPlugin {
     private static HiveRanks instance;
+    private Metrics metrics;
 
     /**
      * Gets the instance of this class.
@@ -26,6 +28,8 @@ public class HiveRanks extends JavaPlugin {
         CachedPlayerManager.getInstance().initializeDirectory();
         ScoreboardManager.getInstance().initializeScoreboard();
 
+        metrics = new Metrics(this);
+
         pluginManager.registerEvents(new PlayerJoinListener(), this);
         getCommand("hiveranks").setExecutor(new HiveRanksCommand());
     }
@@ -33,5 +37,14 @@ public class HiveRanks extends JavaPlugin {
     @Override
     public void onDisable() {
 
+    }
+
+    /**
+     * Gets the Metrics object.
+     *
+     * @return metrics
+     */
+    public Metrics getMetrics() {
+        return metrics;
     }
 }
